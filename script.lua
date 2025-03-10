664bad9494a3c104b40714ec4d0dc2fa5ebb6a9d
vcxris <c.druce123@gmail.com>
2025-03-10 00:38:49 +0000

    commit

--- script.lua
+++ script.lua
@@ -1,3 +1,23 @@
-local test = "Tester"
+local Players = game:GetService("Players")
 
-print(test)
+local plrs = {}
+
+getgenv.Print = true
+
+Players.PlayerAdded:Connect(function(player)
+    plrs[player.Name] = player
+end)
+
+
+Players.PlayerRemoved:Connect(function(player)
+    plrs[player.Name] = nil
+end)
+
+task.spawn(function()
+    while getgenv.Print do
+        task.wait(1)
+        for i,v in plrs do
+            print(i)
+        end
+    end  
+end)
